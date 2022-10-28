b6091dfc0245e16880caf6644046434eb68766b7
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-27 22:26:30 -0300

    fix: axios baseUrl for production

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const environment =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5173'
+    : 'https://ignite-feed-eta.vercel.app/';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:5173',
+  baseURL: environment,
 });
