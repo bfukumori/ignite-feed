510107a533074b627a46b1695f83d3483d01915b
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-27 22:20:30 -0300

    fix:worker  await import

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { AuthProvider } from './context/AuthContext';
+import { worker } from './mocks/browser';
 
 import './global.css';
 
-const { worker } = await import('./mocks/browser');
 worker.start();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
